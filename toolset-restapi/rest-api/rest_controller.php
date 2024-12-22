8151a76096f32330489361849be98b7b00b8acf7
Julien Del Rio <julien.delrio@gmail.com>
2024-12-22 12:29:56 +0100

    Dynamically register a REST route for each selected custom post type
    - Added dynamic registration of REST routes for each custom post type defined in user settings.
    - Can list all items
    - Enhanced readability and maintainability of API endpoints.
    
    Routes are now dynamically created, providing a clean and intuitive URL structure for each custom post type.

--- toolset-restapi/rest-api/rest_controller.php
+++ toolset-restapi/rest-api/rest_controller.php
@@ -15,11 +15,25 @@ class Toolset_RestAPI_Controller {
     public function register_routes() {
         $base_url = get_option( 'toolset_restapi_base_url', 'toolset-data' );
         $version = 'v1';
-        register_rest_route( $base_url . '/' . $version, '/custom-types', array(
+
+        // Route for getting selected types
+        register_rest_route( $base_url . '/' . $version, '/types', array(
             'methods'  => 'GET',
             'callback' => array( $this, 'get_selected_custom_types' ),
             'permission_callback' => '__return_true',
         ));
+
+        // Dynamically register a route for each selected custom post type
+        $selected_post_types = get_option('toolset_restapi_selected_post_types', array());
+        foreach ($selected_post_types as $type) {
+            register_rest_route( $base_url . '/' . $version, '/' . $type . '/', array(
+                'methods'  => 'GET',
+                'callback' => function($request) use ($type) {
+                    return $this->get_custom_type_items_dynamic($type);
+                },
+                'permission_callback' => '__return_true',
+            ));
+        }
     }
 
     /**
@@ -48,6 +62,47 @@ class Toolset_RestAPI_Controller {
         }
 
         return new WP_REST_Response( $custom_types, 200 );
+    }
+
+    /**
+     * Get all items of a custom post type dynamically.
+     *
+     * @param string $type The custom post type.
+     * @return WP_REST_Response
+     */
+    public function get_custom_type_items_dynamic($type) {
+        // Check if the type is allowed
+        $selected_post_types = get_option('toolset_restapi_selected_post_types', array());
+        if (!in_array($type, $selected_post_types)) {
+            return new WP_REST_Response(array('message' => 'Custom type not allowed.'), 403);
+        }
+
+        // Fetch posts of the specified type
+        $query = new WP_Query(array(
+            'post_type'      => $type,
+            'posts_per_page' => -1,
+            'post_status'    => 'publish',
+        ));
+
+        if (!$query->have_posts()) {
+            return new WP_REST_Response(array('message' => 'No items found.'), 404);
+        }
+
+        $items = array();
+        while ($query->have_posts()) {
+            $query->the_post();
+
+            $items[] = array(
+                'id'    => get_the_ID(),
+                'title' => get_the_title(),
+                'link'  => get_permalink(),
+            );
+        }
+
+        // Restore global post data
+        wp_reset_postdata();
+
+        return new WP_REST_Response($items, 200);
     }
 }
 
